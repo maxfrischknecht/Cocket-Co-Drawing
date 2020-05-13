e9dc235ef094575eec7b3158b4dc1a12c4526b4b
Max Frischknecht <max@debutdebut.com>
2020-05-13 11:16:56 +0200

    add socket io

--- public/sketch.js
+++ public/sketch.js
@@ -3,5 +3,6 @@ function setup() {
 }
 
 function draw() {
-
+	background(51);
+	ellipse(mouseX, mouseY, 50, 50);
 }
--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 console.log("app is running");
+
+let express = require('express');
+let app = express();
+let server = app.listen(3000);
+
+// host everything in that public directory
+app.use(express.static('public'));
