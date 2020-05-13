8a5f206f8293fe44c9081729b10374e568a09d7f
Max Frischknecht <max@debutdebut.com>
2020-05-13 11:49:14 +0200

    setup code for co-drawing

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,40 @@
+// THE CLIENT
+let socket;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	background(51);
+
+	// open the connection to the server
+	socket = io();
+	// recieve new information from server
+	socket.on('mouse', newDrawing);
+}
+
+// draw the incoming data
+function newDrawing(data){
+	noStroke();
+	fill(0, 0, 255);
+	ellipse(data.x, data.y, 59, 50)
+}
+
+// draw data that also goes out
+function mouseDragged(){
+	console.log(mouseX, mouseY);
+
+	let data = {
+		x: mouseX, 
+		y: mouseY
+	}
+	// send from client to server
+	socket.emit('mouse', data);
+
+	noStroke();
+	fill(255, 0, 0);
+	ellipse(mouseX, mouseY, 50, 50);
+
+
 }
 
 function draw() {
-	background(51);
-	ellipse(mouseX, mouseY, 50, 50);
 }
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// THE SERVER
 console.log("app is running");
 
 let express = require('express');
@@ -6,3 +7,23 @@ let server = app.listen(3000);
 
 // host everything in that public directory
 app.use(express.static('public'));
+
+let socket = require('socket.io');
+let io = socket(server);
+
+// first event, new connection
+io.on('connection', newConnection);
+
+// a new conncetions creates a socket
+function newConnection(socket){
+  console.log("new conncection: ", socket.id)
+
+  // recieve from client
+  socket.on('mouse', mouseMsg);
+  // do something with the data
+  function mouseMsg(data){
+    // send the message back out! (so others can recieve it) (exluding me)
+    socket.broadcast.emit('mouse', data)
+    console.log(data);
+  }
+}
